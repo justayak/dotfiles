a73ab07afc6e389350a3d09770d4963db81727c0
root <jutanke@googlemail.com>
2016-04-14 11:53:14 +0000

    improve documentation

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+# to install all vundle packages: execute in vim
+# :PluginInstall
 mkdir vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
 git clone https://github.com/kien/ctrlp.vim.git vim/bundle/ctrlp.vim
